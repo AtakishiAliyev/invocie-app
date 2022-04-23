cb50176d57e4b6efe6c972a9e63132eb8a1af84c
Atakishi <atakishieliyev@gmail.com>
2022-04-23 17:41:32 +0400

    Done

--- js/app.js
+++ js/app.js
@@ -1,35 +1,26 @@
-<<<<<<< HEAD
-=======
-const example = [
-    { 
-        "id": "RG0314", 
-        "createdAt": "2021-09-24", 
-        "paymentDue": "2021-10-01", 
-        "description": "Website Redesign", 
-        "paymentTerms": 7, 
-        "clientName": "John Morrison", 
-        "clientEmail": "jm@myco.com", 
-        "status": "paid", 
-        "senderAddress": { 
-            "street": "19 Union Terrace", 
-            "city": "London", 
-            "postCode": "E1 3EZ", 
-            "country": "United Kingdom" 
-        }, 
-        "clientAddress": { 
-            "street": "79 Dover Road", 
-            "city": "Westhall", "postCode": 
-            "IP19 3PF", "country": "United Kingdom" 
-        }, 
-        "items": [
-            { 
-                "name": "Website Redesign", 
-                "quantity": 1, 
-                "price": 14002.33, 
-                "total": 14002.33 
-            }
-        ], 
-        "total": 14002.33 
-    }, 
-]
->>>>>>> 0370d5e3458128f482cc65f49decc04dd803f97b
+const btn = document.querySelector('.btn-main');
+const form = document.querySelector('.form-wrapper');
+const overlay = document.querySelector('.form-overlay');
+const select = document.querySelectorAll('.custom-select');
+
+btn.addEventListener('click', () => {
+    form.classList.add('form-visible');
+})
+
+overlay.addEventListener('click', () => {
+    form.classList.remove('form-visible');
+})
+
+select.forEach(select => {
+    select.addEventListener('click', function() {
+        const selected = this;
+        selected.querySelector(".select-dropdown").classList.toggle('active');
+        const options = selected.querySelectorAll('.select-option');
+        
+        options.forEach(option => {
+            option.addEventListener('click', function() {
+                selected.querySelector(".select-value").innerText = this.innerText;
+            })
+        })
+    })
+})
---
+++ js/test.js
@@ -0,0 +1,33 @@
+const example = [
+    { 
+        "id": "RG0314", 
+        "createdAt": "2021-09-24", 
+        "paymentDue": "2021-10-01", 
+        "description": "Website Redesign", 
+        "paymentTerms": 7, 
+        "clientName": "John Morrison", 
+        "clientEmail": "jm@myco.com", 
+        "status": "paid", 
+        "senderAddress": { 
+            "street": "19 Union Terrace", 
+            "city": "London", 
+            "postCode": "E1 3EZ", 
+            "country": "United Kingdom" 
+        }, 
+        "clientAddress": { 
+            "street": "79 Dover Road", 
+            "city": "Westhall", 
+            "postCode": "IP19 3PF", 
+            "country": "United Kingdom" 
+        }, 
+        "items": [
+            { 
+                "name": "Website Redesign", 
+                "quantity": 1, 
+                "price": 14002.33, 
+                "total": 14002.33 
+            }
+        ], 
+        "total": 14002.33 
+    }, 
+]
