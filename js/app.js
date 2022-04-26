c3870b41c2b4af64fb0b0ac8d72895d2482bae13
atakishialiyev <atakishieliyev@gmail.com>
2022-04-26 23:04:13 +0400

    fix css problems

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const btn = document.querySelector('.btn-main');
 const form = document.querySelector('.form-wrapper');
 const overlay = document.querySelector('.form-overlay');
 const select = document.querySelectorAll('.custom-select');
-let addItemBtn=document.querySelector(".add-item");
+let addItemBtn = document.querySelector(".add-item");
 
 btn.addEventListener('click', () => {
     form.classList.add('form-visible');
@@ -13,14 +13,14 @@ overlay.addEventListener('click', () => {
 })
 
 select.forEach(select => {
-    select.addEventListener('click', function() {
+    select.addEventListener('click', function () {
         const selected = this;
         selected.querySelector(".select-dropdown").classList.toggle('active');
         selected.querySelector(".select-value i").classList.toggle('active');
         const options = selected.querySelectorAll('.select-option');
-        
+
         options.forEach(option => {
-            option.addEventListener('click', function() {
+            option.addEventListener('click', function () {
                 selected.querySelector(".select-value span").innerText = this.innerText;
             })
         })
@@ -28,29 +28,29 @@ select.forEach(select => {
 })
 
 // Add Item
-addItemBtn.addEventListener("click",addItem);
+addItemBtn.addEventListener("click", addItem);
 
-function addItem(){
-    let itemParent=document.querySelector(".item-datalist");
-    let item=document.querySelector(".item-data").cloneNode(true);
-    item.querySelector("#item").value="New Item";
-    item.querySelector("#qty").value="0";
-    item.querySelector("#price").value="0";
-    item.querySelector("#total").value="0.00";
+function addItem() {
+    let itemParent = document.querySelector(".item-datalist");
+    let item = document.querySelector(".item-data").cloneNode(true);
+    item.querySelector("#item").value = "New Item";
+    item.querySelector("#qty").value = "0";
+    item.querySelector("#price").value = "0";
+    item.querySelector("#total").value = "0.00";
     itemParent.append(item);
     removeItem();
 }
 
 // Remove Item
-function removeItem(){
-    let deleteItemBtn=document.querySelectorAll(".delete");
-    deleteItemBtn.forEach(del=>{
-    del.addEventListener("click",function(){
-        console.log(document.querySelectorAll(".delete").length)
-        if(document.querySelectorAll(".delete").length>1){
-        console.log(del)
-        del.closest(".item-data").remove();
-        }
-    })
+function removeItem() {
+    let deleteItemBtn = document.querySelectorAll(".delete");
+    deleteItemBtn.forEach(del => {
+        del.addEventListener("click", function () {
+            console.log(document.querySelectorAll(".delete").length)
+            if (document.querySelectorAll(".delete").length > 1) {
+                console.log(del)
+                del.closest(".item-data").remove();
+            }
+        })
     })
 }
