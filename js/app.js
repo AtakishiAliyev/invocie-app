a2512baea48bf7875f022f4abbf6dca221ace8c8
Kamran Aliyev <kamraneliyev002@gmail.com>
2022-04-26 22:27:10 +0400

    add new item created

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const btn = document.querySelector('.btn-main');
 const form = document.querySelector('.form-wrapper');
 const overlay = document.querySelector('.form-overlay');
 const select = document.querySelectorAll('.custom-select');
+let addItemBtn=document.querySelector(".add-item");
 
 btn.addEventListener('click', () => {
     form.classList.add('form-visible');
@@ -25,3 +26,31 @@ select.forEach(select => {
         })
     })
 })
+
+// Add Item
+addItemBtn.addEventListener("click",addItem);
+
+function addItem(){
+    let itemParent=document.querySelector(".item-datalist");
+    let item=document.querySelector(".item-data").cloneNode(true);
+    item.querySelector("#item").value="New Item";
+    item.querySelector("#qty").value="0";
+    item.querySelector("#price").value="0";
+    item.querySelector("#total").value="0.00";
+    itemParent.append(item);
+    removeItem();
+}
+
+// Remove Item
+function removeItem(){
+    let deleteItemBtn=document.querySelectorAll(".delete");
+    deleteItemBtn.forEach(del=>{
+    del.addEventListener("click",function(){
+        console.log(document.querySelectorAll(".delete").length)
+        if(document.querySelectorAll(".delete").length>1){
+        console.log(del)
+        del.closest(".item-data").remove();
+        }
+    })
+    })
+}
